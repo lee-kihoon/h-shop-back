6b7e6b56222f2595177507d1ca39088e79faa733
lee-kihoon <raindrop5393@naver.com>
2023-05-22 14:07:42 +0900

    쇼핑몰 사이드 프로젝트-백엔드

---
+++ src/main/java/com/h/hshopback/HShopBackApplication.java
@@ -0,0 +1,13 @@
+package com.h.hshopback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HShopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HShopBackApplication.class, args);
+	}
+
+}
